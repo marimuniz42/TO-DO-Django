502b702475d48002a0eee10709bf1a8658f66e36
Mariana <marian.muniz.rocha@gmail.com>
2024-07-12 19:20:40 -0300

    Colocando ForeignKey

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -14,11 +14,11 @@ def taskList(request):
 
     if search:
 
-        tasks = Task.objects.filter(title__icontains=search)
+        tasks = Task.objects.filter(title__icontains=search, user=request.user)
 
     else:
 
-        tasks_list = Task.objects.all().order_by('-created_at')
+        tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
 
         paginator = Paginator(tasks_list, 3)
 
@@ -41,6 +41,7 @@ def newTask(request):
         if form.is_valid():
             task = form.save(commit=False)
             task.done = 'doing'
+            task.user = request.user
             task.save()
             return redirect('/')
 
