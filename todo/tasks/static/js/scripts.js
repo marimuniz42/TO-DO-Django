fd2705eb17cf8e29f921f0f219b102a4d67f94aa
Mariana <marian.muniz.rocha@gmail.com>
2024-07-12 20:06:11 -0300

    Projeto finalizado

--- todo/tasks/static/js/scripts.js
+++ todo/tasks/static/js/scripts.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
 
+    var baseUrl   = 'http://localhost:8000/';
     var deleteBtn = $('.delete-btn');
     var searchBtn = $('#search-btn');
     var searchForm = $('#search-form');
+    var filter     = $('#filter');
     
     $(deleteBtn).on('click', function(e) {
 
@@ -23,4 +25,9 @@ $( document ).ready(function() {
         searchForm.submit();
     });
 
+    $(filter).change(function() {
+        var filter = $(this).val();
+        window.location.href = baseUrl + '?filter=' + filter;
+    });
+    
 });
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from django.contrib import messages
+import datetime
 
 from .models import Task
 
@@ -11,10 +12,17 @@ from .models import Task
 def taskList(request):
 
     search = request.GET.get('search')
+    filter = request.GET.get('filter')
+    tasksDoneRecently = Task.objects.filter(done='done', updated_at__gt=datetime.datetime.now()-datetime.timedelta(days=30)).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
 
     if search:
 
         tasks = Task.objects.filter(title__icontains=search, user=request.user)
+
+    elif filter:
+        tasks = Task.objects.filter(done=filter, user=request.user)
 
     else:
 
@@ -26,7 +34,8 @@ def taskList(request):
 
         tasks = paginator.get_page(page)
 
-    return render(request, 'tasks/list.html', {'tasks': tasks})
+    return render(request, 'tasks/list.html', 
+            {'tasks':tasks, 'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing })
 
 @login_required
 def taskView(request, id):
