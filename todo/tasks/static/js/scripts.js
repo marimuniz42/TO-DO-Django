ae31e2086ea822dab55f6d5ca26a431d5cdd89e8
Mariana <marian.muniz.rocha@gmail.com>
2024-07-12 14:37:08 -0300

    Fazendo o botão de deletar ter confirmação do usuário

--- todo/tasks/static/js/scripts.js
+++ todo/tasks/static/js/scripts.js
@@ -1,1 +1,20 @@
-console.log('funcionou!');
+$( document ).ready(function() {
+
+    var deleteBtn = $('.delete-btn');
+    
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        console.log('Botão de deletar clicado');
+        
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+
+    });
+
+});
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from django.contrib import messages
@@ -6,7 +7,14 @@ from django.contrib import messages
 from .models import Task
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskView(request, id):
@@ -44,7 +52,7 @@ def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
 
-    messages.info(request, 'Tarefa deletada com sucesso')
+    messages.info(request, 'Tarefa deletada com sucesso.')
 
     return redirect('/')
 
